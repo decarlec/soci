d8a638e409fa92e4ba2467150434d5a58698f506
cdecarle <decarle.c@gmai.com>
2025-03-09 21:07:51 -0400

    added welcome page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to Soci</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	const mobileX = '375px';
+	const mobileY = '667px';
+</script>
+
+
+<svelte:head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Tomorrow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</svelte:head>
+<div class="mobile-container" style="width: {mobileX}; height: {mobileY};" >
+  <div class="message">Welcome to Soci</div>
+</div>
+
+<style>
+	.mobile-container {
+    width: var(--width);
+    height: var(--height);
+		border: 2px solid #ccc;
+    place-content: center;
+    
+    .message {
+      font-size: x-large;
+      font-weight: 500;
+      font-family: 'Tomorrow';
+      text-align: center;
+    }
+	}
+</style>
