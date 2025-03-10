7a8adba72258b9193b17e5a3dc4db49b5b3b9992
cdecarle <decarle.c@gmai.com>
2025-03-09 21:35:57 -0400

    started animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,15 @@
-<script>
+<script lang="ts">
+  import { cubicOut } from 'svelte/easing';
 	const mobileX = '375px';
 	const mobileY = '667px';
-</script>
 
+	// function spin(node, { duration }) {
+	// 	return {
+	// 		duration,
+	// 		css: (t, u) => ``
+	// 	};
+	// }
+</script>
 
 <svelte:head>
 <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -10,13 +17,11 @@
 <link href="https://fonts.googleapis.com/css2?family=Tomorrow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </svelte:head>
 <div class="mobile-container" style="width: {mobileX}; height: {mobileY};" >
-  <div class="message">Welcome to Soci</div>
+  <div class="message">Welcome to Soci<br>-&gt;</div>
 </div>
 
 <style>
 	.mobile-container {
-    width: var(--width);
-    height: var(--height);
 		border: 2px solid #ccc;
     place-content: center;
     
